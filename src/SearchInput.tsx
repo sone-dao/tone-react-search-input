a7f17bbba09b26718e8ca0138c2c7ac62839fa1a
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-08 12:56:13 -0500

    fix: optional isMobile

--- src/SearchInput.tsx
+++ src/SearchInput.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './SearchInput.module.scss'
 
 export interface ISearchInputProps {
-  isMobile: boolean
+  isMobile?: boolean
 }
 
 const SearchInput: React.FC<ISearchInputProps> = ({ isMobile = false }) => {
